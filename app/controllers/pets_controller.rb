3a9826024ea75367a01757327239430b2a74008f
Steve Albonico <salbonico@gmail.com>
2019-02-10 21:03:09 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-
+binding.pry 
     @pet = Pet.create(:name => params[:pet_name])
 
     if !params[:pet][:owner_ids][0].empty?
