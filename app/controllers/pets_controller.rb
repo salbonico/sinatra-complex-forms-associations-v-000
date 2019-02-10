ae56975727665d1b4f8074ad5619304d8473a6da
Steve Albonico <salbonico@gmail.com>
2019-02-10 21:06:40 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,10 +11,10 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-binding.pry 
+binding.pry
     @pet = Pet.create(:name => params[:pet_name])
 
-    if !params[:pet][:owner_ids][0].empty?
+    if !params[:pet]= nil
     @pet.owner_id = params[:pet][:owner_ids][0]
     @pet.save
     end
